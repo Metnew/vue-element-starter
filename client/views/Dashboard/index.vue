a9f5eb3365282e4b023c3358d2d1dfda0dbe6247
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-04-30 01:27:46 +0300

    reformat + add some text in views/Dashboard

--- client/views/Dashboard/index.vue
+++ client/views/Dashboard/index.vue
@@ -1,14 +1,15 @@
 <template>
     <div>
-        <h1>Dashboard</h1>
+        <h2>Hi, I'm a "Vue-Element-Starter".</h2>
+        <h3>You can find info
+            <a href="https://github.com/Metnew/vue-element-starter">about me here.</a>
+        </h3>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Dashboard',
-    components: {
-        //     Counter
-    }
+  name: 'Dashboard',
+  components: {}
 }
 </script>
