019c3ccb37481fc5abd5ba137f9e60418c55dc40
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-04-30 01:27:57 +0300

    reformat About

--- client/views/About/index.vue
+++ client/views/About/index.vue
@@ -1,12 +1,13 @@
 <template>
 <div>
     <h1>About page</h1>
+    <a href="https://github.com/Metnew/vue-element-starter">Cash me on Github how bow dah?</a>
 </div>
 </template>
 
 <script>
 export default {
-    name: 'About',
-    components: {}
+  name: 'About',
+  components: {}
 }
 </script>
