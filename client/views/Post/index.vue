38d9c459c7f96e6262bcb548d782265e487d96d7
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-04-30 01:27:18 +0300

    format code, add link to webpack tools

--- client/views/Post/index.vue
+++ client/views/Post/index.vue
@@ -1,22 +1,23 @@
 <template>
 <div>
     <h1>Post page with id: {{postId}}</h1>
+    <a href="https://hackernoon.com/webpack-loaders-and-plugins-for-your-new-progressive-web-app-378e09f469">
+        Maybe you want to improve your Progressive Web App with more Webpack tools?
+    </a>
 </div>
 </template>
 
 <script>
 export default {
-    name: 'Post',
-    data(){
-        return {}
-    },
-    computed: {
-        postId() {
-            return this.$route.params.id
-        }
-    },
-    components: {
-        //     Counter
+  name: 'Post',
+  data () {
+    return {}
+  },
+  computed: {
+    postId () {
+      return this.$route.params.id
     }
+  },
+  components: {}
 }
 </script>
