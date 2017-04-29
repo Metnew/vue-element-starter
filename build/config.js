cd524bcb43a25f63688bb9972be3f6d0f00460e4
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-04-30 01:25:02 +0300

    rewrite config.js, remove stuff for .babelrc, remove postcss, add more deps to vendor

--- build/config.js
+++ build/config.js
@@ -1,5 +1,5 @@
-'use strict'
-const pkg = require('../package')
+'use strict';
+const pkg = require('../package');
 
 module.exports = {
   port: 4000,
@@ -8,23 +8,5 @@ module.exports = {
   // otherwise only set to absolute path when you're using history mode
   publicPath: '/',
   // add these dependencies to a standalone vendor bundle
-  vendor: [
-    'vue', 'vuex', 'vue-router', 'vuex-router-sync', 'whatwg-fetch'
-  ],
-  // disable babelrc by default
-  babel: {
-    babelrc: false,
-    presets: [
-      'vue-app', 'stage-3'
-    ]
-  },
-  postcss: [
-    // add prefix via postcss since it's faster
-    require('autoprefixer')({
-      // Vue does not support ie 8 and below
-      browsers: ['last 2 versions', 'ie > 8']
-    }),
-    require('postcss-nested')
-  ],
-  cssModules: false
-}
+  vendor: ['vue', 'vuex', 'vue-router', 'vuex-router-sync', 'whatwg-fetch', 'normalize.css', 'offline-plugin/runtime']
+};
